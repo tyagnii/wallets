ecab6da2af7fed04aa022f41bfc22156aaa4ba3f
tyagnii <aleksandr.tyagni@gmail.com>
2024-10-09 22:58:46 +0300

    Add ent for maintaining DB

---
+++ gen/ent/schema/wallets.go
@@ -0,0 +1,20 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+type Wallet struct {
+	ent.Schema
+}
+
+// Fields of the Audio.
+func (Wallet) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("UUID").
+			NotEmpty().Unique(),
+		field.Int("amount").
+			Default(0),
+	}
+}
