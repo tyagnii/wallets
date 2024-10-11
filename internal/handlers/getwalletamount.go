101b2d65f2092b8d46d3cd660bfba562b0d6c5b1
tyagnii <aleksandr.tyagni@gmail.com>
2024-10-11 14:06:13 +0300

    Fix Typo; Remove junk comments

--- internal/handlers/getwalletamount.go
+++ internal/handlers/getwalletamount.go
@@ -6,12 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// GetWalletAmount
-//
-// TODO:
-// Add db connection
-// Hide models at internal/models
-// log through gin server logger
+// GetWalletBalance returns wallet balance
 func (h *Handler) GetWalletBalance(c *gin.Context) {
 	uuid := c.Params.ByName("UUID")
 	c.JSON(http.StatusOK, gin.H{"uuid": uuid, "value": "some amount returned"})
